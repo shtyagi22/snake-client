b690b4a0d5b80676c13a5b9a78386cae941ccd75
shtyagi22 <sh.tyagi22@gmail.com>
2022-08-03 19:24:30 -0400

    refactored code to make use of module and exports

---
+++ client.js
@@ -0,0 +1,24 @@
+
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  conn.on("connect", () => {
+    //conn.write("Hello from client!");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+
+  return conn;
+};
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,27 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243", // IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  conn.on("connect", () => {
-    //conn.write("Hello from client!");
-  });
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
